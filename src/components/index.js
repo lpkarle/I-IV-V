f9dccfd00090dace04d5f449bb1414baf44f8e84
Lukas Karle <lukas.karle@t-online.de>
2020-10-06 20:11:27 +0200

    📝 updated readme and gitignore

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,3 @@ export { default as Fretboard } from './Fretboard/Fretboard';
 export { default as CircleOfFifths } from './CircleOfFifths/CircleOfFifths';
 export { default as Home } from './Home/Home';
 export { default as MusicTheory } from './MusicTheory/MusicTheory';
-export { default as Footer } from './Footer/Footer';
