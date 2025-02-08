5024d15f8204ed3ffaff02dea3acdeb843f10db4
Lukas Karle <28999846+lpkarle@users.noreply.github.com>
2025-02-08 14:28:02 +0100

    Refactor css location.

--- app/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-5xl">I IV V</h1>
-    </>
-  );
-}
-
-export default App;
---
+++ app/src/components/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-sans">I IV V</h1>
+
+      <button className="btn">Hellp</button>
+    </>
+  );
+}
+
+export default App;
